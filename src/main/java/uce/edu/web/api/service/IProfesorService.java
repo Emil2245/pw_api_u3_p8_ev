4d9e2120c4c5c056f3266b77878c6f100e4880eb
Kevin Andrade <130163890+kaandradec@users.noreply.github.com>
2025-07-04 14:36:18 -0500

    Tarea12

--- src/main/java/uce/edu/web/api/service/IProfesorService.java
+++ src/main/java/uce/edu/web/api/service/IProfesorService.java
@@ -1,12 +1,15 @@
 package uce.edu.web.api.service;
 
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.UriInfo;
 import uce.edu.web.api.repository.model.Estudiante;
 import uce.edu.web.api.repository.model.Profesor;
+import uce.edu.web.api.service.to.ProfesorTO;
 
 import java.util.List;
 
 public interface IProfesorService {
-    public Profesor buscarProfesorId(Integer id);
+    ProfesorTO buscarProfesorId(Integer id, @Context UriInfo uriInfo);
 
     List<Profesor> buscarTodos();
     void guardar(Profesor profesor);
